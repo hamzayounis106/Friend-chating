62529f45e5f89763ccfccc6ec682df247268b1a1
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2025-03-01 02:49:08 +0500

    Update the job post submission if the user is sending mail to himself then stop him in the frontend

--- src/components/AddJobPostButtonForm.tsx
+++ src/components/AddJobPostButtonForm.tsx
@@ -8,6 +8,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { addJobValidator } from '@/lib/validations/add-Job';
 import { useSession } from 'next-auth/react';
+import { toast } from 'react-hot-toast';
 
 interface AddJobButtonProps {}
 
@@ -32,103 +33,128 @@ const AddJobPostButtonForm: FC<AddJobButtonProps> = () => {
         ...data,
         date: new Date(data.date).toISOString(), // Convert date to ISO
         createdBy: session?.data?.user?.id, // Replace with actual user ID
-        patientId:session?.data?.user?.id, // Replace with actual patient ID
+        patientId: session?.data?.user?.id, // Replace with actual patient ID
         surgeonEmails: data.surgeonEmails, // Already a comma-separated string
         videoURLs: data.videoURLs, // Already a comma-separated string
       };
-  
-      console.log("Validated Data:", transformedData);
-  
-      await axios.post("/api/Jobs/add", transformedData);
+
+      console.log('Validated Data:', transformedData);
+
+      await axios.post('/api/Jobs/add', transformedData);
       setShowSuccessState(true);
     } catch (error) {
-      console.error("Error:", error); // Log the full error object
-  
+      console.error('Error:', error); // Log the full error object
+
       if (error instanceof z.ZodError) {
         error.errors.forEach((err) => {
           setError(err.path[0] as keyof FormData, { message: err.message });
         });
         return;
       }
-  
+
       if (error instanceof AxiosError) {
-        console.error("Axios Error Response:", error.response?.data); // Log the Axios error response
-        setError("title", { message: error.response?.data || "Server error." });
+        console.error('Axios Error Response:', error.response?.data); // Log the Axios error response
+        setError('title', { message: error.response?.data || 'Server error.' });
         return;
       }
-  
-      setError("title", { message: "Something went wrong." });
+
+      setError('title', { message: 'Something went wrong.' });
     }
   };
   const onSubmit = (data: FormData) => {
+    const userEmail = session.data?.user.email;
+    console.log(userEmail);
+    const containEmail = data?.surgeonEmails.some(
+      (item) => item.email === userEmail
+    );
+    if (containEmail) {
+      toast.error(`You can't invite yourself to this post`);
+      return;
+    }
     addJob(data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="max-w-sm space-y-4">
-      <label className="block text-sm font-medium text-gray-900">Job Title</label>
+    <form onSubmit={handleSubmit(onSubmit)} className='max-w-sm space-y-4'>
+      <label className='block text-sm font-medium text-gray-900'>
+        Job Title
+      </label>
       <input
         {...register('title')}
-        type="text"
-        className="block w-full rounded-md border py-1.5 text-gray-900 shadow-sm"
-        placeholder="Job Title"
+        type='text'
+        className='block w-full rounded-md border py-1.5 text-gray-900 shadow-sm'
+        placeholder='Job Title'
       />
-      <p className="text-sm text-red-600">{errors.title?.message}</p>
+      <p className='text-sm text-red-600'>{errors.title?.message}</p>
 
-      <label className="block text-sm font-medium text-gray-900">Job Type</label>
+      <label className='block text-sm font-medium text-gray-900'>
+        Job Type
+      </label>
       <input
         {...register('type')}
-        type="text"
-        className="block w-full rounded-md border py-1.5 text-gray-900 shadow-sm"
-        placeholder="Job Type"
+        type='text'
+        className='block w-full rounded-md border py-1.5 text-gray-900 shadow-sm'
+        placeholder='Job Type'
       />
-      <p className="text-sm text-red-600">{errors.type?.message}</p>
+      <p className='text-sm text-red-600'>{errors.type?.message}</p>
 
-      <label className="block text-sm font-medium text-gray-900">Job Date</label>
+      <label className='block text-sm font-medium text-gray-900'>
+        Job Date
+      </label>
       <input
         {...register('date')}
-        type="date"
-        className="block w-full rounded-md border py-1.5 text-gray-900 shadow-sm"
+        type='date'
+        className='block w-full rounded-md border py-1.5 text-gray-900 shadow-sm'
       />
-      <p className="text-sm text-red-600">{errors.date?.message}</p>
+      <p className='text-sm text-red-600'>{errors.date?.message}</p>
 
-      <label className="block text-sm font-medium text-gray-900">Job Description</label>
+      <label className='block text-sm font-medium text-gray-900'>
+        Job Description
+      </label>
       <textarea
         {...register('description')}
-        className="block w-full rounded-md border py-1.5 text-gray-900 shadow-sm"
-        placeholder="Job Description"
+        className='block w-full rounded-md border py-1.5 text-gray-900 shadow-sm'
+        placeholder='Job Description'
       />
-      <p className="text-sm text-red-600">{errors.description?.message}</p>
+      <p className='text-sm text-red-600'>{errors.description?.message}</p>
 
-      <label className="block text-sm font-medium text-gray-900">Surgeon Emails (comma separated)</label>
+      <label className='block text-sm font-medium text-gray-900'>
+        Surgeon Emails (comma separated)
+      </label>
       <input
         {...register('surgeonEmails')}
-        type="text"
-        className="block w-full rounded-md border py-1.5 text-gray-900 shadow-sm"
-        placeholder="email1@example.com, email2@example.com"
+        type='text'
+        className='block w-full rounded-md border py-1.5 text-gray-900 shadow-sm'
+        placeholder='email1@example.com, email2@example.com'
       />
-      <p className="text-sm text-red-600">{errors.surgeonEmails?.message}</p>
+      <p className='text-sm text-red-600'>{errors.surgeonEmails?.message}</p>
 
-      <label className="block text-sm font-medium text-gray-900">Video URLs (comma separated)</label>
+      <label className='block text-sm font-medium text-gray-900'>
+        Video URLs (comma separated)
+      </label>
       <input
         {...register('videoURLs')}
-        type="text"
-        className="block w-full rounded-md border py-1.5 text-gray-900 shadow-sm"
-        placeholder="https://example.com/video1, https://example.com/video2"
+        type='text'
+        className='block w-full rounded-md border py-1.5 text-gray-900 shadow-sm'
+        placeholder='https://example.com/video1, https://example.com/video2'
       />
-      <p className="text-sm text-red-600">{errors.videoURLs?.message}</p>
+      <p className='text-sm text-red-600'>{errors.videoURLs?.message}</p>
 
-      <label className="block text-sm font-medium text-gray-900">Agree to Terms</label>
+      <label className='block text-sm font-medium text-gray-900'>
+        Agree to Terms
+      </label>
       <input
         {...register('agreeToTerms')}
-        type="checkbox"
-        className="rounded-md border py-1.5 text-gray-900 shadow-sm"
+        type='checkbox'
+        className='rounded-md border py-1.5 text-gray-900 shadow-sm'
       />
-      <p className="text-sm text-red-600">{errors.agreeToTerms?.message}</p>
+      <p className='text-sm text-red-600'>{errors.agreeToTerms?.message}</p>
 
-      <Button type="submit">Add Job</Button>
+      <Button type='submit'>Add Job</Button>
 
-      {showSuccessState && <p className="text-sm text-green-600">Job request sent!</p>}
+      {showSuccessState && (
+        <p className='text-sm text-green-600'>Job request sent!</p>
+      )}
     </form>
   );
 };
