ec2b6ef4174faffea9792cb9f0f769432bb984d2
Hamza Younis <hamzayounis106@gmail.com>
2025-03-06 04:41:12 +0500

    SIngle Job Post Page PRODUCTION will Fail

---
+++ src/app/api/Jobs/[jobid]/route.ts
@@ -0,0 +1,23 @@
+import Job from '@/app/models/Job';
+import dbConnect from '@/lib/db';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest, { params }: { params: { jobid: string } }) {
+  const { jobid } = params; // Change from jobId to jobid
+  console.log("received id", jobid);
+
+  await dbConnect();
+
+  try {
+    const job = await Job.findById(jobid).populate('patientId').lean().exec();
+
+    if (!job) {
+      return NextResponse.json({ error: 'Job not found' }, { status: 404 });
+    }
+
+    return NextResponse.json({ job }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
